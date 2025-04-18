4584f479c7138e0cb47d04a0ce3f078d2378cd96
Vladimir Vlasenko <v.g.vlasenko@yandex.ru>
2025-04-18 17:08:54 +0300

    Add logic in comments for functions

--- src/main/kotlin/com/vlasenko/kokodi_test/domain/GameSession.kt
+++ src/main/kotlin/com/vlasenko/kokodi_test/domain/GameSession.kt
@@ -3,14 +3,16 @@ package com.vlasenko.kokodi_test.domain
 import com.vlasenko.kokodi_test.domain.enums.GameStatus
 import jakarta.persistence.*
 import org.springframework.context.annotation.Scope
+import org.springframework.transaction.annotation.Transactional
 
 /**
  * Game session entity
  */
 
 @Entity
-@Table(name = "games")
+@Table(name = "sessions")
 //@Scope("prototype")
+// implements Runnable and use it like Thread ?
 class GameSession(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -46,11 +48,23 @@ class GameSession(
     var skipCounter: Int = 0
 ) {
 
+    @Transactional
+    // rename to run() and add @Override annotation ?
     fun nextTurn() {
         TODO("1st turn at start and finish turn when first player reached 30 scores.")
+        // set active user from user list. if = 0 it is 1st turn
+        // check to skip or not. If skip - set skip counter -1 and continue
+        // take card from deck
+        // apply card effect
+        // delete card from deck
+        // add card to turns history
+        // check scores to reach 30 points, if false - call new turn, if true - set status to FINISHED and call closeAndSave()
     }
 
     fun closeAndSave() {
         TODO("After game ended save entity and close it.")
+        // deck.trimToSize()
+        // turns.trimToSize()
+        // save this GameSession
     }
 }
